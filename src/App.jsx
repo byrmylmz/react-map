ac28d0254c376de7c3af0dd8cb45fd35c9efa47f
byrmylmz <bayramyilmaz061@gmail.com>
2023-03-16 18:27:43 +0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import signal from "./assets/wifi-signal.png";
+import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { Icon } from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+function App() {
+  const position = [40.99812, 39.77223];
+  const position1 = [40.99805, 39.77170];
+
+  const customIcon = new Icon({
+    iconUrl: signal,
+    iconSize: [33, 33],
+  });
+
+  return (
+    <div className="App">
+      <div className="map" id="map">
+        <MapContainer center={position} scrollWheelZoom={true} zoom={19} >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            maxZoom={22}
+            maxNativeZoom={19}
+          />
+
+          <Marker position={position} icon={customIcon}>
+            <Popup>Library 1</Popup>
+          </Marker>
+
+          <Marker position={position1} icon={customIcon}>
+            <Popup>Library 2</Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
