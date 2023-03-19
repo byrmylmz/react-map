1702363d15f297ddc831412875f2f9db558b187a
byrmylmz <bayramyilmaz061@gmail.com>
2023-03-19 21:19:34 +0300

    update

--- src/Components/FloorMap.jsx
+++ src/Components/FloorMap.jsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 
 const FloorMap = () => {
     return ( 
-        <h1>Floor map</h1>
+        <div className='h-screen bg-gradient-to-r from-cyan-500 to-blue-500'>
+            <h1 className='text-center text-white text-5xl align-text-bottom font-semibold font-sans'>Floor map</h1>
+        </div>
      );
 }
  
--- src/Components/HeatMap.jsx
+++ src/Components/HeatMap.jsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 
 const HeatMap = () => {
     return ( 
-        <h1>heatmap</h1>
+        <div className='h-screen bg-gradient-to-r from-cyan-500 to-blue-500'>
+            <h1 className='text-center text-white text-5xl align-text-bottom font-semibold font-sans'>Heat map</h1>
+        </div>
      );
 }
  
