1cbbe83258429ef0ffdcaf2493d71dc1ea60826d
byrmylmz <bayramyilmaz061@gmail.com>
2023-03-19 21:21:16 +0300

    update

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -3,7 +3,7 @@ const TodoList = () => {
         <>
         
        
-        <ul className="w-96 text-left text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+        <ul className="w-80 text-left text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
             <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
                 <div className="flex items-center pl-3">
                     <input checked id="vue-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
