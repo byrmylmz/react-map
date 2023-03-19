af5d6b100372e7f725a5a717d3e28c6e424d166a
byrmylmz <bayramyilmaz061@gmail.com>
2023-03-19 13:43:27 +0300

    update

--- src/Components/Dasboard.jsx
+++ src/Components/Dasboard.jsx
@@ -12,23 +12,25 @@ export default function Example() {
         <body class="h-full">
         ```
       */}
-      <main className="grid min-h-full place-items-center bg-white py-24 px-6 sm:py-32 lg:px-8 ">
-        <div className="text-center">
-       
-          <h1 className="drop-shadow-md font-sans mt-4 text-3xl font-bold tracking-tight text-blue-700 sm:text-5xl mb-5">Maps Todo</h1>
-            <TodoList/>
+      <div className="h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
+          <main className="grid align-top place-items-center py-24 px-6 sm:py-32 lg:px-8  ">
+            <div className="text-center ">
           
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <Link
-              to={`/normalmap`}
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Go to map
-            </Link>
-           
-          </div>
-        </div>
-      </main>
+              <h1 className="drop-shadow-md font-sans mt-4 text-3xl font-bold tracking-tight text-gray-100 sm:text-5xl mb-5">Maps Todo</h1>
+                <TodoList/>
+          
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <Link
+                  to={`/normalmap`}
+                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Go to map
+                </Link>
+          
+              </div>
+            </div>
+          </main>
+      </div>
     </>
   )
 }
